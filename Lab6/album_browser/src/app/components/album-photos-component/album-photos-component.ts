2b8ce7a304b3bab083967a101c227a880c150797
Anu <a_salauatov@kbtu.kz>
2026-03-03 16:41:08 +0500

    ю

--- Lab6/album_browser/src/app/components/album-photos-component/album-photos-component.ts
+++ Lab6/album_browser/src/app/components/album-photos-component/album-photos-component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AlbumService } from '../../services/album-service'; // <-- здесь имя файла точно как у тебя
+import { CommonModule } from '@angular/common';
+import { RouterModule, ActivatedRoute } from '@angular/router';
+import { AlbumService } from '../../services/album-service';
 import { Photo } from '../../models/photo.model';
 
 @Component({
   selector: 'app-album-photos',
   templateUrl: './album-photos-component.html',
-  styleUrls: ['./album-photos-component.css']
+  styleUrls: ['./album-photos-component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule], // важно для *ngFor и routerLink
 })
 export class AlbumPhotosComponent implements OnInit {
+  albumId!: number;
+  albumPhotos = signal<Photo[]>([]);
+  loading = signal(true);
+
   private route = inject(ActivatedRoute);
   private albumService = inject(AlbumService);
 
-  albumId!: number;
-  albumPhotos = signal<Photo[]>([]);
-  loading = signal<boolean>(true);
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.albumId = Number(this.route.snapshot.paramMap.get('id'));
-
     this.albumService.getAlbumPhotos(this.albumId).subscribe({
       next: (data) => {
         this.albumPhotos.set(data);
         this.loading.set(false);
       },
-      error: () => this.loading.set(false)
+      error: () => this.loading.set(false),
     });
   }
 }
--- Lab6/album_browser/src/main.ts
+++ Lab6/album_browser/src/main.ts
@@ -1,26 +1,52 @@
-// main.ts
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { RouterModule, provideRouter } from '@angular/router';
+// src/main.ts
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideRouter, RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { AlbumDetailComponent } from './components/album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './components/album-photos-component/album-photos-component';
+
+// Настройка маршрутов
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'albums/:id', component: AlbumDetailComponent },
+  { path: 'albums/:id/photos', component: AlbumPhotosComponent },
+];
+
+// Запуск приложения
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+// AppModule для main.ts
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-import { HomeComponent } from './app/components/home/home.component';
-import { AboutComponent } from './app/components/about/about.component';
-import { AlbumsComponent } from './app/components/albums/albums.component';
-import { AlbumDetailComponent } from './app/components/album-detail/album-detail.component';
-import { AlbumPhotosComponent } from './app/components/album-photos-component/album-photos-component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(HttpClientModule, RouterModule),
-    provideRouter([
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'albums', component: AlbumsComponent },
-      { path: 'albums/:id', component: AlbumDetailComponent },
-      { path: 'albums/:id/photos', component: AlbumPhotosComponent }
-    ])
-  ]
-});
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
